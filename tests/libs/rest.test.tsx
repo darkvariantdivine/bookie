e618d6f9e606f72a5a203a904818e531a5c983a4
David Lee <ltw_david@u.nus.edu>
2023-03-06 02:31:22 +0800

    Fixing broken API unit tests

--- tests/libs/rest.test.tsx
+++ tests/libs/rest.test.tsx
@@ -8,7 +8,7 @@ import {
   loginUser,
   logoutUser,
   updateBooking,
-  API,
+  API, RestApiError,
 } from "@/libs/rest";
 import {
   test,
@@ -21,6 +21,10 @@ import {
 import USER from '@/mocks/user.json';
 import ROOMS from '@/mocks/rooms.json';
 import BOOKINGS from '@/mocks/bookings.json';
+import {
+  AxiosError,
+  AxiosResponse
+} from "axios";
 
 describe("Mocking Login API calls", () => {
 
@@ -52,14 +56,16 @@ describe("Mocking Login API calls", () => {
     };
     let testInput = {username: 'test', password: 'hello_world'};
     API.post = jest.fn();
-    (API.post as jest.Mock).mockResolvedValue({
-      data: mockError,
-      status: 401
-    });
+    (API.post as jest.Mock).mockRejectedValue(new AxiosError(
+      mockError.message,
+      'test',
+      undefined,
+      undefined,
+      {data: mockError, status: 401} as AxiosResponse
+    ));
 
-    const output = await loginUser(testInput);
-    expect(output['data']).toEqual(mockError);
-    expect(output['status']).toEqual(401);
+    await expect(loginUser(testInput)).rejects.toThrow(Error);
+    await expect(loginUser(testInput)).rejects.toThrow('Requesting user does not exist');
     expect((API.post as jest.Mock).mock.calls[0][0]).toEqual(`/login`);
     expect((API.post as jest.Mock).mock.calls[0][1]).toEqual(testInput);
   });
@@ -77,7 +83,7 @@ describe("Mocking Login API calls", () => {
     expect((API.delete as jest.Mock).mock.calls).toHaveLength(1);
     expect((API.delete as jest.Mock).mock.calls[0][0]).toEqual(`/login`);
     expect((API.delete as jest.Mock).mock.calls[0][1]).toEqual(
-      {headers: {Authorization: `Bearer ${token}`, 'Content-Type': 'application/json'}}
+      {headers: {Authorization: `Bearer ${token}`}}
     );
   });
 
@@ -146,31 +152,32 @@ describe("CRUD bookings", () => {
     expect((API.post as jest.Mock).mock.calls[0][0]).toEqual(`/bookings`);
     expect((API.post as jest.Mock).mock.calls[0][1]).toEqual(booking);
     expect((API.post as jest.Mock).mock.calls[0][2]).toEqual(
-      {headers: {Authorization: `Bearer ${token}`, 'Content-Type': 'application/json'}}
+      {headers: {Authorization: `Bearer ${token}`}}
     );
   });
 
   test('Create overlapping booking', async () => {
     let token: string = '4448fdf20043d0a8bc5d4383ebac1a64';
     let { id, user, lastModified, ...booking} = BOOKINGS[0];
-    let mockError: RestApiError = {
+    let mockError: IRestApiError = {
       code: 400,
       message: "Booking overlaps with other Bookings",
     };
     API.post = jest.fn();
-    (API.post as jest.Mock).mockResolvedValue({
-      data: mockError,
-      status: 400
-    });
+    (API.post as jest.Mock).mockRejectedValue(new AxiosError(
+      mockError.message,
+      'test',
+      undefined,
+      undefined,
+      {data: mockError, status: 400} as AxiosResponse
+    ));
 
-    const output = await createBooking(booking, token);
-    expect(output['data']).toEqual(mockError);
-    expect(output['status']).toEqual(400);
-    expect((API.post as jest.Mock).mock.calls).toHaveLength(1);
+    await expect(createBooking(booking, token)).rejects.toThrow(Error);
+    await expect(createBooking(booking, token)).rejects.toThrow('Booking overlaps with other Bookings');
     expect((API.post as jest.Mock).mock.calls[0][0]).toEqual(`/bookings`);
     expect((API.post as jest.Mock).mock.calls[0][1]).toEqual(booking);
     expect((API.post as jest.Mock).mock.calls[0][2]).toEqual(
-      {headers: {Authorization: `Bearer ${token}`, 'Content-Type': 'application/json'}}
+      {headers: {Authorization: `Bearer ${token}`}}
     );
   });
 
@@ -189,37 +196,39 @@ describe("CRUD bookings", () => {
     expect((API.put as jest.Mock).mock.calls[0][0]).toEqual(`/bookings/${id}`);
     expect((API.put as jest.Mock).mock.calls[0][1]).toEqual({start: start, duration: duration});
     expect((API.put as jest.Mock).mock.calls[0][2]).toEqual(
-      {headers: {Authorization: `Bearer ${token}`, 'Content-Type': 'application/json'}}
+      {headers: {Authorization: `Bearer ${token}`}}
     );
   });
 
   test('Update overlapping booking', async () => {
     let token: string = '4448fdf20043d0a8bc5d4383ebac1a64';
     let { id, start, duration, ..._} = BOOKINGS[0];
-    let mockError: RestApiError = {
+    let mockError: IRestApiError = {
       code: 400,
       message: "Booking overlaps with other Bookings",
     };
     API.put = jest.fn();
-    (API.put as jest.Mock).mockResolvedValue({
-      data: mockError,
-      status: 400
-    });
+    (API.put as jest.Mock).mockRejectedValue(new AxiosError(
+      mockError.message,
+      'test',
+      undefined,
+      undefined,
+      {data: mockError, status: 400} as AxiosResponse
+    ));
 
-    const output = await updateBooking(id, {start: start, duration: duration}, token);
-    expect(output['data']).toEqual(mockError);
-    expect(output['status']).toEqual(400);
-    expect((API.put as jest.Mock).mock.calls).toHaveLength(1);
+
+    await expect(updateBooking(id, {start: start, duration: duration}, token)).rejects.toThrow(Error);
+    await expect(updateBooking(id, {start: start, duration: duration}, token)).rejects.toThrow('Booking overlaps with other Bookings');
     expect((API.put as jest.Mock).mock.calls[0][0]).toEqual(`/bookings/${id}`);
     expect((API.put as jest.Mock).mock.calls[0][1]).toEqual({start: start, duration: duration});
     expect((API.put as jest.Mock).mock.calls[0][2]).toEqual(
-      {headers: {Authorization: `Bearer ${token}`, 'Content-Type': 'application/json'}}
+      {headers: {Authorization: `Bearer ${token}`}}
     );
   });
 
   test('Cancel bookings', async () => {
     let token: string = '4448fdf20043d0a8bc5d4383ebac1a64';
-    let bookings: string[] = BOOKINGS.map((booking: Booking) => {return booking.id}) as string[];
+    let bookings: string[] = BOOKINGS.map((booking: IBooking) => {return booking.id}) as string[];
     API.delete = jest.fn();
     (API.delete as jest.Mock).mockResolvedValue({
       status: 204
@@ -234,21 +243,22 @@ describe("CRUD bookings", () => {
 
   test('Unable to cancel bookings', async () => {
     let token: string = '4448fdf20043d0a8bc5d4383ebac1a64';
-    let bookings: string[] = BOOKINGS.map((booking: Booking) => {return booking.id}) as string[];
-    let mockError: RestApiError = {
+    let bookings: string[] = BOOKINGS.map((booking: IBooking) => {return booking.id}) as string[];
+    let mockError: IRestApiError = {
       code: 500,
       message: 'Unable to delete Booking(s)'
     };
     API.delete = jest.fn();
-    (API.delete as jest.Mock).mockResolvedValue({
-      data: mockError,
-      status: 500
-    });
+    (API.delete as jest.Mock).mockRejectedValue(new AxiosError(
+      mockError.message,
+      'test',
+      undefined,
+      undefined,
+      {data: mockError, status: 500} as AxiosResponse
+    ));
 
-    const output = await deleteBookings(bookings, token);
-    expect(output['data']).toEqual(mockError);
-    expect(output['status']).toEqual(500);
-    expect((API.delete as jest.Mock).mock.calls).toHaveLength(1);
+    await expect(deleteBookings(bookings, token)).rejects.toThrow(Error);
+    await expect(deleteBookings(bookings, token)).rejects.toThrow('Unable to delete Booking(s)');
     expect((API.delete as jest.Mock).mock.calls[0][0]).toEqual(`/bookings`);
   });
 
